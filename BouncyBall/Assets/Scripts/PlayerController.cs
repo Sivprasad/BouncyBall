4bf3dbfa79700f5b9f3efb66746434e28a960d03
Sivaprasad <sivaprasad.g@live.com>
2019-01-03 01:05:47 +0530

    Player code updated

--- BouncyBall/Assets/Scripts/PlayerController.cs
+++ BouncyBall/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+   private float speed;
+   private  Rigidbody rb;
     void Start()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody>().AddForce(-transform.up * 500);
+           
+        }
+         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
+        this.speed = 0.01f;
+       
+            if (transform.position.y < 1)
+            {
+              transform.Translate(new Vector3(0, 6.0f, 0) * speed);
+              rb.useGravity = true;
+            }
+    }
+
+
+    
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().AddForce(-transform.up * 500);
         
     }
 }
